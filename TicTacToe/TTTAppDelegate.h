655c64ac1e8dc1e62a60a2adfb6aa99cddbf6d23
Yi-Wei Chia <yiweic@amazon.com>
2014-08-25 20:46:25 -0700

    Initial Commit

---
+++ TicTacToe/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  TicTacToe
+//
+//  Created by Chia, Yi-Wei on 8/25/14.
+//  Copyright (c) 2014 yiweic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Chia, Yi-Wei on 8/25/14.
+//  Copyright (c) 2014 yiweic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTTAppDelegate class]));
+    }
+}
